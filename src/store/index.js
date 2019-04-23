845ac68c4bb3b584f5e5fbb1a465ff668605c07f
wsd <4508718444@qq.com>
2019-04-23 11:46:33 +0800

    wsd

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,5 @@
 import Vue from "vue";
 import Vuex from "vuex";
-<<<<<<< HEAD
-import shopApply from './shopApply.js'
-import shops from './shops.js'
-import ProModule from './ProModule.js'
-import serviceModule from './serviceModule.js'
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  modules:{
-=======
 import shopApply from "./shopApply.js";
 import shops from "./shops.js";
 import ProModule from "./ProModule.js";
@@ -20,7 +10,6 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
     orderModule,
->>>>>>> b996af92ad674f0c2e766fc49a85874f4787b0ef
     shopApply,
     shops,
     ProModule,
