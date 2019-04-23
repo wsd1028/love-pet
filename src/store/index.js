a83b167d68dba70a462c982a14c32286ee39496e
wsd <4508718444@qq.com>
2019-04-23 23:00:02 +0800

    吴少冬完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 Vue.use(Vuex);
 
->>>>>>> 8b2836c622a7c642439f49142a88f1666cfdfd74
 import shopApply from "./shopApply.js";
 import shops from "./shops.js";
 import ProModule from "./ProModule.js";
@@ -13,21 +9,15 @@ import serviceModule from "./serviceModule.js";
 import allServiceModule from "./allServiceModule.js";
 import orderModule from "./orderModule";
 import usersModule from "./usersModule.js";
-<<<<<<< HEAD
-=======
 import ProModule from "./ProModule.js";
 import serviceModule from "./serviceModule.js";
 import userSysModule from "./userSysModule.js";
->>>>>>> wangjing
-=======
 import supplierModule from './supplierModule.js'
 
->>>>>>> 8b2836c622a7c642439f49142a88f1666cfdfd74
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-<<<<<<< HEAD
     orderModule,
     shopApply,
     shops,
@@ -35,11 +25,7 @@ export default new Vuex.Store({
     serviceModule,
     supplierModule,
     allServiceModule,
-    usersModule
-=======
-    ProModule,
-    serviceModule,
+    usersModule,
     userSysModule
->>>>>>> wangjing
   }
 });
