6d3a43ef3878b1562df67886c6dddf98d76df66a
Yang <1045063806@qq.com>
2019-04-23 10:30:13 +0800

    dev修改router

--- src/router.js
+++ src/router.js
@@ -4,17 +4,10 @@ import VueRouter from "vue-router";
 import Login from "./components/login";
 import Register from "./components/register";
 import Manage from "./components/manage";
-<<<<<<< HEAD
 import Order from "./components/order";
 import Sent from "./components/Sent";
 import PendingTrade from "./components/PendingTrade";
 import PendingServe from "./components/PendingServe";
-=======
-import Sent from "./components/sent";
-import PendingTrade from "./components/pendingTrade";
-import PendingServe from "./components/pendingServe";
-import CompletedServe from "./components/completedServe";
->>>>>>> 4dca283f7190582e30282cd0802918d6870c5944
 import PetMaster from "./components/petMaster";
 import Product from "./components/product";
 import AllService from "./components/allService";
@@ -40,15 +33,10 @@ const routes = [
       { path: "/manage/userSys", component: UserSys },
       { path: "/manage/petMaster", component: PetMaster },
       { path: "/manage/shopSys", component: ShopSys },
-<<<<<<< HEAD
       { path: "/manage/supplier", component: Supplier },
       { path: "/manage/shops", component: Shops },
       { path: "/manage/audit", component: Audit },
       { path: "/manage/blackShops", component: BlackShops },
-
-=======
-      { path: "/manage/supplier", component: Supplier }
->>>>>>> 4dca283f7190582e30282cd0802918d6870c5944
     ]
   },
   {
@@ -56,16 +44,6 @@ const routes = [
     component: ShopManage,
     children: [
       { path: "/shopManage/product", component: Product },
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-      { path: "/shopManage/service", component: Service },
-      { path: "/shopManage/completedServe", component: CompletedServe },
-      { path: "/shopManage/sent", component: Sent },
-      { path: "/shopManage/pendingTrade", component: PendingTrade },
-      { path: "/shopManage/pendingServe", component: PendingServe }
-=======
->>>>>>> 4dca283f7190582e30282cd0802918d6870c5944
       { path: "/shopManage/allService", component: AllService },
       { path: "/shopManage/serviceType", component: ServiceType },
       {
@@ -77,10 +55,6 @@ const routes = [
           { path: "/shopManage/order/PendingServe", component: PendingServe }
         ]
       }
-<<<<<<< HEAD
-=======
->>>>>>> 1e3789ef98d5162cc682f1cae281e9dcc8d16737
->>>>>>> 4dca283f7190582e30282cd0802918d6870c5944
     ]
   }
 ];
