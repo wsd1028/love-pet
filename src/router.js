c96445719ed1bd29a10db4cfdca526168ca8ae60
wsd <4508718444@qq.com>
2019-04-23 11:42:49 +0800

    wsd

--- src/router.js
+++ src/router.js
@@ -17,17 +17,9 @@ import ShopManage from "./components/shopManage";
 import ShopSys from "./components/shopSys";
 import Supplier from "./components/supplier";
 import UserSys from "./components/userSys";
-<<<<<<< HEAD
-import Audit from './components/audit'
-import Shops from './components/shops'
-import Order from './components/order'
-
-import BlackShops from './components/blackShops'
-=======
 import Audit from "./components/audit";
 import Shops from "./components/shops";
 import BlackShops from "./components/blackShops";
->>>>>>> b996af92ad674f0c2e766fc49a85874f4787b0ef
 
 Vue.use(VueRouter);
 const routes = [
@@ -45,19 +37,10 @@ const routes = [
       { path: "/manage/userSys", component: UserSys },
       { path: "/manage/petMaster", component: PetMaster },
       { path: "/manage/shopSys", component: ShopSys },
-<<<<<<< HEAD
-      { path: "/manage/supplier", component: Supplier },
-      { path: "/manage/supplier", component: Supplier },
-      { path: "/manage/shops", component: Shops },
-      { path: "/manage/audit", component: Audit },
-      { path: "/manage/blackShops", component: BlackShops },
-      { path: "/manage/supplier", component: Supplier }
-=======
       { path: "/manage/supplier", component: Supplier },
       { path: "/manage/shops", component: Shops },
       { path: "/manage/audit", component: Audit },
       { path: "/manage/blackShops", component: BlackShops }
->>>>>>> b996af92ad674f0c2e766fc49a85874f4787b0ef
     ]
   },
   {
@@ -65,29 +48,12 @@ const routes = [
     component: ShopManage,
     children: [
       { path: "/shopManage/product", component: Product },
-<<<<<<< HEAD
-      { path: "/shopManage/order", component: Order },
-=======
->>>>>>> b996af92ad674f0c2e766fc49a85874f4787b0ef
       { path: "/shopManage/completedServe", component: CompletedServe },
       { path: "/shopManage/sent", component: Sent },
       { path: "/shopManage/pendingTrade", component: PendingTrade },
       { path: "/shopManage/pendingServe", component: PendingServe },
       { path: "/shopManage/allService", component: AllService },
-<<<<<<< HEAD
-      { path: "/shopManage/serviceType", component: ServiceType },
-      {
-        path: "/shopManage/order",
-        component: Order,
-        children: [
-          { path: "/shopManage/order/Sent", component: Sent },
-          { path: "/shopManage/order/PendingTrade", component: PendingTrade },
-          { path: "/shopManage/order/PendingServe", component: PendingServe }
-        ]
-      }
-=======
       { path: "/shopManage/serviceType", component: ServiceType }
->>>>>>> b996af92ad674f0c2e766fc49a85874f4787b0ef
     ]
   }
 ];
