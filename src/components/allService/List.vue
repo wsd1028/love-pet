ef3aaa1d1461897674dc1658885543f89f931d21
wsd <4508718444@qq.com>
2019-04-23 18:22:58 +0800

    吴少冬第二天

---
+++ src/components/allService/List.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-table :data="services" style="width: 100%">
+      <el-table-column label="名称" width="130" align="center">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="类型" width="130" align="center">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.serviceType.name }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="开放时间段" width="130" align="center">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.schedule }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="耗时" width="130" align="center">
+      <template slot-scope="scope">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ scope.row.useTime }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="价格" width="130" align="center">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">￥{{ scope.row.price }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="服务人员" width="130" align="center">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{ scope.row.waiter }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button type="primary" icon="el-icon-edit" circle @click="handleEdit(scope.row._id)"></el-button>
+        <el-button type="danger" icon="el-icon-delete" circle @click="del(scope.row._id)"></el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import { createNamespacedHelpers} from "vuex";
+const { mapActions, mapState, mapMutations } = createNamespacedHelpers(
+  "allServiceModule"
+);
+export default {
+computed: {
+    ...mapState(["shopId","services","pagenation"])
+  },
+  created() {
+    let shopId = this.shopId;
+    this.getServices({shopId})
+  },
+  methods: {
+      ...mapActions(["getServices","deleteServices"]),
+       del(id) {
+      this.$confirm("此操作将永久删除该服务, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.deleteServices(id);
+          this.getServices({
+            page: this.pagenation.curpage,
+            shopId: this.shopId
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
