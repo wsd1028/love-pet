2031655878ac10302304eb7414d95bf9f244b0ee
Your Name <you@example.com>
2019-04-24 16:00:45 +0800

    王彬第三天

--- src/router.js
+++ src/router.js
@@ -20,39 +20,6 @@ import Shops from "./components/shops";
 import BlackShops from "./components/blackShops";
 
 Vue.use(VueRouter);
-<<<<<<< HEAD
-const routes = [{
-    path: "/",
-    component: Login
-  },
-  {
-    path: "/register",
-    component: Register
-  },
-  {
-    path: "/shopApply",
-    component: ShopApply
-  },
-  {
-    path: "/manage",
-    component: Manage,
-    children: [{
-        path: "/manage/userSys",
-        component: UserSys
-      },
-      {
-        path: "/manage/petMaster",
-        component: PetMaster
-      },
-      {
-        path: "/manage/shopSys",
-        component: ShopSys
-      },
-      {
-        path: "/manage/supplier",
-        component: Supplier
-      }
-=======
 const routes = [
   { path: "/", component: Manage },
   { path: "/register", component: Register },
@@ -72,42 +39,11 @@ const routes = [
       { path: "/manage/shops", component: Shops },
       { path: "/manage/audit", component: Audit },
       { path: "/manage/blackShops", component: BlackShops }
->>>>>>> 93fb3d14181419a52146e81944b071d1b9c4228a
     ]
   },
   {
     path: "/shopManage",
     component: ShopManage,
-<<<<<<< HEAD
-    children: [{
-        path: "/shopManage/product",
-        component: Product
-      },
-      {
-        path: "/shopManage/completedServe",
-        component: CompletedServe
-      },
-      {
-        path: "/shopManage/allService",
-        component: AllService
-      },
-      {
-        path: "/shopManage/serviceType",
-        component: ServiceType
-      },
-      {
-        path: "/shopManage/order/Sent",
-        component: Sent
-      },
-      {
-        path: "/shopManage/order/PendingTrade",
-        component: PendingTrade
-      },
-      {
-        path: "/shopManage/order/PendingServe",
-        component: PendingServe
-      }
-=======
     children: [
       { path: "/shopManage/product", component: Product },
       { path: "/shopManage/sent", component: Sent },
@@ -116,7 +52,6 @@ const routes = [
       { path: "/shopManage/pendingServe", component: PendingServe },
       { path: "/shopManage/allService", component: AllService },
       { path: "/shopManage/serviceType", component: ServiceType }
->>>>>>> 93fb3d14181419a52146e81944b071d1b9c4228a
     ]
   }
 ];
