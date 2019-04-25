a7409f832b47899989549abf1cacf139b62d9115
hqy <1094676506@qq.com>
2019-04-25 10:12:08 +0800

    hqy宠主修改

--- src/components/allServiceSaleStatistics/index.vue
+++ src/components/allServiceSaleStatistics/index.vue
@@ -11,7 +11,16 @@
     </div>
 </template>
 <script>
-
+import echarts from "echarts/lib/echarts";
+// 引入柱状图
+import "echarts/lib/chart/bar"
+import "echarts/lib/chart/pie";
+import "echarts/lib/chart/scatter";
+// 引入提示框和标题组件
+import "echarts/lib/component/tooltip";
+import "echarts/lib/component/title";
+import "echarts/lib/component/legend";
+import "echarts/extension/bmap/bmap";
 import axios from "axios";
 import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 
