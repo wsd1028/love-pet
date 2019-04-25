60f166996dfe72e5be5d2a06fe472869227939e8
WYD <1285386741@qq.com>
2019-04-25 09:00:24 +0800

    更新

---
+++ src/components/Sent/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-card class="box-card">
+   <div slot="header" class="clearfix">
+      <SentSearch/>
+    </div>
+    <SentList/>
+    <SentPage/>
+  </el-card>
+</template>
+
+<script>
+import SentSearch from "./SentSearch";
+import SentList from './SentList';
+import SentPage from './SentPage'
+export default {
+  components: {
+    SentSearch,
+    SentList,
+    SentPage
+  },
+  methods: {},
+  data() {
+    return {}
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/completedServe/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <CompletedServeSearch/>
+    </div>
+    <CompletedServeList/>
+    <CompletedServePage/>
+  </el-card>
+</template>
+
+<script>
+import CompletedServeSearch from "./CompletedServeSearch";
+import CompletedServeList from './CompletedServeList'
+import CompletedServePage from './CompletedServePage'
+export default {
+  components: {
+    CompletedServeSearch,
+    CompletedServeList,
+    CompletedServePage
+  },
+  methods: {
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+<style scoped>
+</style>
